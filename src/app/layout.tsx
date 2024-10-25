2b63833f39ffe25fa1305d88e88e8bce472d58d3
Bekzod Tukhtasinov <bektuxtasinov@gmail.com>
2024-10-25 15:50:49 +0500

    create project layout

---
+++ src/app/layout.tsx
@@ -0,0 +1,29 @@
+import { Toaster } from "@/components/ui/toaster";
+import { cn } from "@/lib/utils/ui-utils";
+import type { Metadata } from "next";
+import { Noto_Sans } from "next/font/google";
+import "./globals.css";
+
+const notoSans = Noto_Sans({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Pulse | Real-time Q&A and Polls",
+  description:
+    "Connect with your audience with real-time Q&A and Polls with Pulse.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="h-full" suppressHydrationWarning>
+      <body className={cn("h-full bg-zinc-100", notoSans.className)}>
+        {children}
+
+        <Toaster />
+      </body>
+    </html>
+  );
+}
