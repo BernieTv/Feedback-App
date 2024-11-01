24a1833c44cba8731ca14d0513bf63fa316d67f5
Bekzod Tukhtasinov <bektuxtasinov@gmail.com>
2024-11-01 17:17:29 +0500

    fetch data by infinite scroll

---
+++ src/lib/actions/get-user-bookmarked-events-action.ts
@@ -0,0 +1,13 @@
+'use server';
+
+import { z } from 'zod';
+
+import { getUserBookmarkedEvents } from '../server/get-user-bookmarked-events';
+import { eventIdSchema } from '../validations/event-schemas';
+import { actionClient } from './safe-action';
+
+export const getUserBookmarkedEventsAction = actionClient
+  .schema(z.object({ cursor: eventIdSchema.optional() }))
+  .action(async ({ parsedInput: { cursor } }) =>
+    getUserBookmarkedEvents({ cursor }),
+  );
